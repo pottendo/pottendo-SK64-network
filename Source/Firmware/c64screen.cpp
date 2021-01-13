e0cb759ccbf65779fb7a8ded9cde9d49b3318be2
hpingel <post@henningpingel.de>
2021-01-13 20:53:28 +0100

    improve joystick navigation on sktx screen (button press triggers enter)

--- Source/Firmware/c64screen.cpp
+++ Source/Firmware/c64screen.cpp
@@ -1813,7 +1813,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		{
 			//if (pSidekickNet->IsRunning())
 			{
-				pSidekickNet->enteringSktpScreen();
+				pSidekickNet->enteringSktpScreen(); //just for the refresh
 				menuScreen = MENU_SYSTEMINFO;
 				handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 				return;
@@ -1879,6 +1879,12 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		*/
 		else
 		{
+			//virtual key press
+			//joystick button press
+			if ( k == 92 )
+			{
+				k = VK_RETURN;
+			}
 			//the user has actually manually pressed a key on the Commodore keyboard
 			pSidekickNet->queueSktpKeypress(k);
 		}
